95ca5ec2166e30913b08fdb4beb60f69e79a26b0
Morgan <gann.gann87@gmail.com>
2021-03-30 19:12:43 +0200

    creation de la table agorawp_users (en cours)

--- includes/class-gannwp-activator.php
+++ includes/class-gannwp-activator.php
@@ -31,6 +31,8 @@ class Gannwp_Activator {
 	public static function activate() {
 
 		Gannwp_Activator::create_gannwp_params();
+		Gannwp_Activator::create_gannwp_users();
+
 	}
 
 
@@ -93,6 +95,67 @@ class Gannwp_Activator {
 		}
 
 	}
+
+	/**
+	* create_gannwp_users. (use period)
+	*
+	* create and seed gannwp_users table.
+	*
+	* @since    1.0.0
+	*/
+	public static function create_gannwp_users() {
+
+		global $wpdb;
+		$alreadyexist;
+		$table_name = $wpdb->prefix . "gannwp_users";
+		$gannwp_db_version = '1.0';
+		$charset_collate = $wpdb->get_charset_collate();
+
+
+		if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+			$alreadyexist = false;
+		}else {
+			$alreadyexist = true;
+		};
+		$foreign = $wpdb->prefix . "users" . "(ID)" ;
+
+		$sql = "CREATE TABLE $table_name (
+			`wp_users_ID` BIGINT UNSIGNED NOT NULL,
+	   `segondID` INT UNSIGNED NOT NULL,
+	   INDEX `fk_wp_gannwp_users_wp_users_idx` (`wp_users_ID` ASC),
+	   PRIMARY KEY (`segondID`),
+	   CONSTRAINT `fk_wp_gannwp_users_wp_users`
+	     FOREIGN KEY (`wp_users_ID`)
+	     REFERENCES `wp_users` (`ID`)
+		) $charset_collate;";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+		dbDelta( $sql );
+		add_option( 'gannwp_db_version', $gannwp_db_version );
+
+		// seed table if new
+		// TODO update db if already exist
+		if (!$alreadyexist) {
+			// $wpdb->insert(
+			// 	$table_name,
+			// 	array(
+			// 		'name' => 'first param',
+			// 		'valueTXT' => 'param text value',
+			// 		'valueINT' => '1',
+			// 	)
+			// );
+
+			// $wpdb->query("INSERT INTO $table_name
+			// 	(`name`, `valueTXT`, `valueINT`)
+			// 	VALUES
+			// 	('name-1', 'val-1', '1'),
+			// 	('name-2', 'val-2', '2'),
+			// 	('name-3', 'val-3', '3')");
+		}
+
+	}
+
 
 
 }
