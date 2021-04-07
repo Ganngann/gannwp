7d3a2473daba81d7b8e40837ce2d77b7dd49b229
Morgan <gann.gann87@gmail.com>
2021-04-08 01:07:11 +0200

    correction liste users

--- admin/partials/gann_userList.php
+++ admin/partials/gann_userList.php
@@ -2,9 +2,10 @@
 
 // 1st Method - Declaring $wpdb as global and using it to execute an SQL query statement that returns a PHP object
 global $wpdb;
-$users = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}users", OBJECT);
 
+$users = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}users", OBJECT);
 $gannwp_users = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}gannwp_users", OBJECT);
+
 $gannwp_users_fields_meta = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}gannwp_users_meta", OBJECT);
 
 // var_dump($gannwp_users);
@@ -14,44 +15,69 @@ $url = admin_url($path);
 // $link = "<a href='{$url}'>Edit</a>";
 // echo $link;
 
-$indexed = array();
-$results = array();
 
-foreach($gannwp_users as $value) {
-   $indexed[$value->userID] = $value;
-}
 
-foreach($users as $obj) {
-   $tempValue = '';
-   if (isset($indexed[$obj->ID])) {
-      echo "string";
-      $tempValue = $indexed[$obj->ID];
+foreach ($gannwp_users as $key => $gannwp_user) {
+   // echo $gannwp_user->userID;
 
-   }
-
-   // var_dump($value);
-   // echo "</br>coucou</br>";
-
-   if ($tempValue != '') {
-      // echo "</br>coucou ca existe</br>";
-
-      foreach($tempValue as $name => $val) {
-         $obj->$name = $val;
+   foreach ($users as $key => $user) {
+      if ($user->ID == $gannwp_user->userID) {
+         foreach ($gannwp_user as $key => $value) {
+            // echo "</br></br>";
+            $user->$key = $value;
+            // var_dump($user->$key);
+         }
+         // var_dump($user);
       }
-      array_push($results, $obj);
-
    }
-
 }
+
+
+
+
+
+
+
+
+//
+// $indexed = array();
+$results = array();
+//
+// foreach($gannwp_users as $value) {
+//    $indexed[$value->userID] = $value;
+// }
+//
+// foreach($users as $obj) {
+//    $tempValue = '';
+//    if (isset($indexed[$obj->ID])) {
+//       // echo "string";
+//       $tempValue = $indexed[$obj->ID];
+//
+//    }
+//
+//    // var_dump($value);
+//    // echo "</br>coucou</br>";
+//
+//    if ($tempValue != '') {
+//       // echo "</br>coucou ca existe</br>";
+//
+//       foreach($tempValue as $name => $val) {
+//          $obj->$name = $val;
+//       }
+//       array_push($results, $obj);
+//
+//    }
+//
+// }
 
 
 
 // $result = array_merge( $gannwp_users, $users );
 
-foreach ($results as $key => $value) {
-   var_dump($value);
-   echo "</br></br>";
-}
+// foreach ($users as $key => $value) {
+// var_dump($value);
+// echo "</br></br>";
+// }
 
 ?>
 <div class="wrap">
@@ -65,14 +91,13 @@ foreach ($results as $key => $value) {
                </th>
             <?php endforeach; ?>
          </thead>
-         <?php foreach ($results as $key => $user) : ?>
+         <?php foreach ($users as $key => $user) : ?>
             <tr>
                <?php foreach ($gannwp_users_fields_meta as $key => $field) : ?>
                   <?php $columnName = $field->columnName; ?>
                   <td>
                      <p>
-                        <?php echo $user->$columnName ?>
-
+                        <?php echo isset($user->$columnName) ? $user->$columnName : ''; ?>
                      </p>
                   </td>
                <?php endforeach; ?>
