a2bfeba95934b10fdeeb0ed3535c66ef4c4c02ec
Morgan <gann.gann87@gmail.com>
2021-04-08 13:41:28 +0200

    change user role (not finish)

--- admin/partials/gann_userList.php
+++ admin/partials/gann_userList.php
@@ -5,84 +5,29 @@ global $wpdb;
 
 $users = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}users", OBJECT);
 $gannwp_users = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}gannwp_users", OBJECT);
-
+$gannwp_users_roles = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}gannwp_users_roles", OBJECT);
 $gannwp_users_fields_meta = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}gannwp_users_meta", OBJECT);
-
-// var_dump($gannwp_users);
 
 $path = 'admin.php?page=gannwp%2Fadmin%2Fpartials%2Fgann_userForm.php';
 $url = admin_url($path);
-// $link = "<a href='{$url}'>Edit</a>";
-// echo $link;
 
 
 
 foreach ($gannwp_users as $key => $gannwp_user) {
-   // echo $gannwp_user->userID;
-
    foreach ($users as $key => $user) {
       if ($user->ID == $gannwp_user->userID) {
          foreach ($gannwp_user as $key => $value) {
-            // echo "</br></br>";
             $user->$key = $value;
-            // var_dump($user->$key);
          }
-         // var_dump($user);
       }
    }
 }
 
-
-
-
-
-
-
-
-//
-// $indexed = array();
-$results = array();
-//
-// foreach($gannwp_users as $value) {
-//    $indexed[$value->userID] = $value;
-// }
-//
-// foreach($users as $obj) {
-//    $tempValue = '';
-//    if (isset($indexed[$obj->ID])) {
-//       // echo "string";
-//       $tempValue = $indexed[$obj->ID];
-//
-//    }
-//
-//    // var_dump($value);
-//    // echo "</br>coucou</br>";
-//
-//    if ($tempValue != '') {
-//       // echo "</br>coucou ca existe</br>";
-//
-//       foreach($tempValue as $name => $val) {
-//          $obj->$name = $val;
-//       }
-//       array_push($results, $obj);
-//
-//    }
-//
-// }
-
-
-
-// $result = array_merge( $gannwp_users, $users );
-
-// foreach ($users as $key => $value) {
-// var_dump($value);
-// echo "</br></br>";
-// }
-
 ?>
+<?php var_dump($_POST)  ?>
 <div class="wrap">
-   <h1>GannWP Agora submenu</h1>
-   <form class="" action=<?php echo $url ?> method="post">
+   <h1>Liste des utilisateurs</h1>
+   <form class="" action="" method="post">
       <table>
          <thead>
             <?php foreach ($gannwp_users_fields_meta as $key => $value) : ?>
@@ -90,29 +35,32 @@ $results = array();
                   <?php echo $value->name ?>
                </th>
             <?php endforeach; ?>
+            <th>
+               Role
+            </th>
          </thead>
          <?php foreach ($users as $key => $user) : ?>
             <tr>
                <?php foreach ($gannwp_users_fields_meta as $key => $field) : ?>
                   <?php $columnName = $field->columnName; ?>
                   <td>
-                     <p>
-                        <?php echo isset($user->$columnName) ? $user->$columnName : ''; ?>
-                     </p>
+                     <?php echo isset($user->$columnName) ? $user->$columnName : ''; ?>
                   </td>
                <?php endforeach; ?>
+               <td>
+                  <input type='hidden' name='field' value='' />
+                  <select class="" name="role" onchange="this.form.field.value=this.name; this.form.submit()">
+                     <option value="" >--Choisissez un role--</option>
+                     <?php foreach ($gannwp_users_roles as $key => $value): ?>
+                        <option value="<?php echo $value->ID ?>" <?php echo isset($user->roleID) && ($value->ID == $user->roleID) ? 'style="font-weight: bold;" selected' : "" ?> >
+                           <?php echo $value->name ?>
+                        </option>
+                     <?php endforeach; ?>
+                  </select>
+
+               </td>
             </tr>
          <?php endforeach; ?>
       </table>
-      <input type="submit" name="" value="test">
-
-      <?php
-      // echo admin_url();
-      ?>
-
-      <?php
-
-      ?>
-
    </form>
 </div>
